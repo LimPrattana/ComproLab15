e47a59c7b00cf4d2045707ef921c3ad51dbfa83b
LimPrattana <vivo.twy0270@gmail.com>
2025-01-31 11:57:52 +0700

    lab15_3 completed

--- lab15_3.cpp
+++ lab15_3.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
+template<class TT>
+TT max_element(TT x[],int N){
+	sort(&x[0],&x[N]);
+	return x[N-1];
+}
+
 
 int main(){
 	int a[] = {1,2,4,6,9}; 
 	double b[] = {5.5,2.1,13,-7,6.9,0,25,11.1,-4}; 
-	
-	pa = max_element(); 
-	cout << "Address of the highest value in array A is " << pa << "\n";
+	int sizeA =sizeof(a)/sizeof(a[0]);
+	int sizeB =sizeof(b)/sizeof(b[0]);
+	for(int i=0; i<sizeA;i++){
+
+	}
+	int pa = max_element(a,sizeA); 
+	cout << "Address of the highest value in array A is " << &pa << "\n";
 	cout << "The highest value in array A is "<< pa << "\n";
 	
-	pb = max_element(); 
-	cout << "Address of the highest value in array B is " << pb << "\n";
+	double pb = max_element(b,sizeB); 
+	cout << "Address of the highest value in array B is " << &pb << "\n";
 	cout << "The highest value in array B is "<< pb << "\n";
 
 	return 0;
